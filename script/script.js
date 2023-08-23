342b004304a74f41453b3e5680a92681fb1888ab
Daniel <danialnasronline@gmail.com>
2023-08-23 13:41:11 +0330

    add clean texts and add modern website load

--- script/script.js
+++ script/script.js
@@ -4,8 +4,26 @@ const msg = {
     qname: "What's Your Name?"
 };
 let num = 0;
+let ii = 0;
 function $(el){
     return document.querySelector(el);
+}
+function typeEffect(el , string){
+    const text = string;
+    const arrayText = text.split("") ;
+    let i = 0;
+    let type = setInterval(()=>{
+        $(el).innerHTML += arrayText[i];
+        i++;
+        if(i == arrayText.length){
+            clearInterval(type);
+        }
+    },60);
+}
+function error(){
+    $("main").innerHTML += `<span class="span" id="error`+ ii +`"></span><br />`;
+    typeEffect("#error" + ii, "Error 404!");
+    ii++;
 }
 let inputname = $("#name");
 function start(){
@@ -20,134 +38,91 @@ function start(){
     $("#i"+num).focus();
     $("#i"+num).addEventListener("keypress",(e)=>{
         if(e.key === "Enter"){
-            if($("#i"+num).value == 1 || $("#i"+num).value == 2 || $("#i"+num).value == 3){
-                let screen = window.innerWidth;
+            if($("#i"+num).value == 1 || $("#i"+num).value == 2 || $("#i"+num).value == 3 || $("#i"+num).value == 4 || $("#i"+num).value == 5){
                 if($("#i"+num).value == 1){
-                    if(screen > 768){
                         $("main").innerHTML += `
                         <br />
                         <br />
                         <div>
-                            <p>const contact = {</p>
-                            <span>&nbsp;&nbsp;&nbsp;&nbsp;Email: "<a href="mailto:me@imdanieldev.ir">me@imdanieldev.ir</a>",</span>
-                            <br />
-                            <span>&nbsp;&nbsp;&nbsp;&nbsp;Telegram: "<a href="//t.me/imdanieln">@imdanieln</a>",</span>
-                            <br />
-                            <span>&nbsp;&nbsp;&nbsp;&nbsp;Telegram Channel: "<a href="//t.me/danieln_ch">@danieln_ch</a>",</span>
-                            <br />
-                            <span>&nbsp;&nbsp;&nbsp;&nbsp;Telegram Gap: "<a href="//t.me/danieln-gap">@danieln_gap</a>",</span>
-                            <br />
-                            <span>&nbsp;&nbsp;&nbsp;&nbsp;Mastodon: "<a href="//mas.to/imdanieln">@imdanieln@mas.to</a>",</span>
-                            <br />
-                            <span>&nbsp;&nbsp;&nbsp;&nbsp;Github: "<a href="//github.com/imdanieldev">imdanieldev</a>"</span>
-                            <p>}</p>
+                        <span>Email: <a href="mailto:me@imdanieldev.ir">me@imdanieldev.ir</a></span>
+                        <br />
+                        <span>Telegram: <a href="//t.me/imdanieln">@imdanieln</a></span>
+                        <br />
+                        <span>Telegram Channel: <a href="//t.me/danieln_ch">@danieln_ch</a></span>
+                        <br />
+                        <span>Telegram Gap: <a href="//t.me/danieln-gap">@danieln_gap</a></span>
+                        <br />
+                        <span>Mastodon: <a href="//mas.to/imdanieln">@imdanieln@mas.to</a></span>
+                        <br />
+                        <span>Github: <a href="//github.com/imdanieldev">imdanieldev</a></span>
                         </div>
                         <br />
                         <br />
                         `;
-                    }else{
-                        $("main").innerHTML += `
-                        <br />
-                        <br />
-                        <div>
-                            <p>const contact = {</p>
-                            <span>Email: "<a href="mailto:me@imdanieldev.ir">me@imdanieldev.ir</a>",</span>
-                            <br />
-                            <span>Telegram: "<a href="//t.me/imdanieln">@imdanieln</a>",</span>
-                            <br />
-                            <span>Telegram Channel: "<a href="//t.me/danieln_ch">@danieln_ch</a>",</span>
-                            <br />
-                            <span>Telegram Gap: "<a href="//t.me/danieln-gap">@danieln_gap</a>",</span>
-                            <br />
-                            <span>Mastodon: "<a href="//mas.to/imdanieln">@imdanieln@mas.to</a>",</span>
-                            <br />
-                            <span>Github: "<a href="//github.com/imdanieldev">imdanieldev</a>"</span>
-                            <p>}</p>
-                        </div>
-                        <br />
-                        <br />
-                        `;
-                    }
                     start();
                 }
                 if($("#i"+num).value == 2){
-                    if(screen > 768){
                         $("main").innerHTML += `
                         <br />
                         <br />
                         <div>
-                            <p>const skills = {</p>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;HTML-CSS: true,</span>
-                                <br>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Js-VueJs: true,</span>
-                                <br>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Bootstrap-TailwindCss: true,</span>
-                                <br>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Git-Github: true</span>
-                            <p>}</p>
+                        <span>HTML-CSS: true,</span>
+                        <br>
+                        <span>Js-VueJs: true,</span>
+                        <br>
+                        <span>Bootstrap-TailwindCss: true,</span>
+                        <br>
+                        <span>Git-Github: true</span>
                         </div>
                         <br />
                         <br />
                         `;
-                    }else{
-                        $("main").innerHTML += `
-                        <br />
-                        <br />
-                        <div>
-                            <p>const skills = {</p>
-                                <span>HTML-CSS: true,</span>
-                                <br>
-                                <span>Js-VueJs: true,</span>
-                                <br>
-                                <span>Bootstrap-TailwindCss: true,</span>
-                                <br>
-                                <span>Git-Github: true</span>
-                            <p>}</p>
-                        </div>
-                        <br />
-                        <br />
-                        `;
-                    }
                     start();
                 }
                 if($("#i"+num).value == 3){
-                    if(screen > 768){
                         $("main").innerHTML += `
                         <br />
                         <br />
                         <div>
-                            <p>const info = {</p>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Full Name: "Daniel Nasr",</span>
-                                <br>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Location: "Iran>isfahan",</span>
-                                <br>
-                                <span>&nbsp;&nbsp;&nbsp;&nbsp;Birthday: "2008/3/7"</span>
-                            <p>}</p>
+                        <span>Full Name: Daniel Nasr</span>
+                        <br>
+                        <span>Location: Iran>isfahan</span>
+                        <br>
+                        <span>Birthday: 2008/3/7</span>
                         </div>
                         <br />
                         <br />
                         `;
-                    }else{
-                        $("main").innerHTML += `
-                        <br />
-                        <br />
-                        <div>
-                            <p>const info = {</p>
-                                <span>&Full Name: "Daniel Nasr",</span>
-                                <br>
-                                <span>Location: "Iran>isfahan",</span>
-                                <br>
-                                <span>Birthday: "2008/3/7"</span>
-                            <p>}</p>
-                        </div>
-                        <br />
-                        <br />
-                        `;
-                    }
                     start();
                 }
+                if($("#i"+num).value == 4){
+                    $("#i"+num).blur();
+                    setTimeout(() => {
+                        $("main").innerHTML = `
+                        <span>Contact Me: 1</span>
+                        <br>
+                        <span>My Skill: 2</span>
+                        <br>
+                        <span>About Me: 3</span>
+                        <br>
+                        <span>Clean Here: 4</span>
+                        <br>
+                        <span>Modern Version: 5</span>
+                        </div>
+                        `;
+                    }, 2000);
+                    setTimeout(() => {
+                        start();
+                    }, 3000);
+                }
+                if($("#i"+num).value == 5){
+                    $("main").innerHTML = '<iframe src="//imdanieldev.ir/portfolio-v2" class="iframe"></iframe>';
+                    $("main").className = "";
+                    $("head").innerHTML -= '<link rel="stylesheet" href="./style/style.css">';
+                    $("head").innerHTML += '<link rel="stylesheet" href="./style/iframe.css">';
+                }
             }else{
-                $("main").innerHTML += '<span class="span">Error 404</span><br />';
+                error();
                 start();
             }
         }
@@ -158,9 +133,9 @@ function showinput(){
     inputname.focus();
     inputname.addEventListener("keypress", (e)=>{
         if(e.key === "Enter"){
-            inputname.blur();
             if(inputname.value != ""){
-                $("#sayhello").innerHTML = "Hello " + inputname.value;
+                inputname.blur();
+                typeEffect("#sayhello", "Hello " + inputname.value)
                 setTimeout(()=>{
                     $("#help").classList.remove("hidden");
                     inputname.classList.add("hidden");
@@ -171,23 +146,11 @@ function showinput(){
     });
 }
 setTimeout(()=>{
-    let firstmsg = new Typed(
-        "#firstmsg", {
-            strings: [msg['hello']],
-            typeSpeed: 60,
-            showCursor: false,
-            onComplete: ()=>{
-                let QName = new Typed(
-                    "#QName", {
-                        strings: [msg['qname']],
-                        typeSpeed: 60,
-                        showCursor: false,
-                        onComplete: ()=>{
-                            showinput();
-                        }
-                    }
-                );
-            }
-        }
-    );
+    typeEffect("#firstmsg", msg['hello'])
+    setTimeout(()=>{
+        typeEffect("#QName", msg['qname'])
+        setTimeout(()=>{
+            showinput();
+        },1000)
+    },1000);
 },400);
